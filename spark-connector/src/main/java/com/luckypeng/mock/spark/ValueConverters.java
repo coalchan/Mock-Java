7890f49d779188e125843bbca01d3b007f8ca300
coalchan <coalchan@gmail.com>
2023-04-12 18:41:39 +0800

    add(spark-connector): 支持 Timestamp 类型

--- spark-connector/src/main/java/com/luckypeng/mock/spark/ValueConverters.java
+++ spark-connector/src/main/java/com/luckypeng/mock/spark/ValueConverters.java
@@ -5,6 +5,7 @@ import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
 import org.apache.spark.unsafe.types.UTF8String;
 
+import java.sql.Timestamp;
 import java.util.function.Function;
 
 /**
@@ -16,6 +17,7 @@ public class ValueConverters {
     public static final Function<String, Float> FLOAT_CONVERTER = value -> value == null ? null : Float.parseFloat(value);
     public static final Function<String, Double> DOUBLE_CONVERTER = value -> value == null ? null : Double.parseDouble(value);
     public static final Function<String, Boolean> BOOLEAN_CONVERTER = value -> value == null ? null : Boolean.parseBoolean(value);
+    public static final Function<String, Long> TIMESTAMP_CONVERTER = value -> value == null ? null : Timestamp.valueOf(value).getTime() * 1000;
     public static final Function<String, UTF8String> UTF8_STRING_CONVERTER = UTF8String::fromString;
 
     public static Function<String, ?>[] getConverters(StructType schema) {
@@ -35,6 +37,8 @@ public class ValueConverters {
                 valueConverters[i] = UTF8_STRING_CONVERTER;
             } else if (field.dataType().equals(DataTypes.BooleanType)) {
                 valueConverters[i] = BOOLEAN_CONVERTER;
+            } else if (field.dataType().equals(DataTypes.TimestampType)) {
+                valueConverters[i] = TIMESTAMP_CONVERTER;
             } else {
                 throw new IllegalArgumentException("暂不支持类型" + field.dataType().typeName());
             }
